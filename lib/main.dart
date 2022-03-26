3025bdfb2b74271945001e46495194e7ce500818
canghafiz <fizrahman47@gmail.com>
2022-03-26 11:31:28 +0700

    Init Sound Recorder

---
+++ lib/main.dart
@@ -0,0 +1,234 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'dart:async';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:record_mp3/record_mp3.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String statusText = "";
+  bool isComplete = false;
+
+  Duration duration = const Duration();
+  Timer? timer;
+
+  void startTimee() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
+  }
+
+  void reset() {
+    setState(() {
+      duration = const Duration();
+      timer?.cancel();
+    });
+  }
+
+  void addTime() {
+    const addSeconds = 1;
+
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+
+      duration = Duration(seconds: seconds);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget buildTime() {
+      String twoDigits(int n) => n.toString().padLeft(2, '0');
+      final hours = twoDigits(duration.inHours);
+      final minutes = twoDigits(duration.inMinutes.remainder(60));
+      final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+      return Text(
+        (hours == "00") ? "$minutes:$seconds" : "$hours:$minutes:$seconds",
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: GestureDetector(
+                  child: Container(
+                    height: 48.0,
+                    decoration: BoxDecoration(color: Colors.red.shade300),
+                    child: Center(
+                      child: Text(
+                        'start',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  onTap: () async {
+                    startRecord();
+                    startTimee();
+                  },
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  child: Container(
+                    height: 48.0,
+                    decoration: BoxDecoration(color: Colors.blue.shade300),
+                    child: Center(
+                      child: Text(
+                        RecordMp3.instance.status == RecordStatus.PAUSE
+                            ? 'resume'
+                            : 'pause',
+                        style:
+                            TextStyle(color: Color.fromARGB(255, 17, 11, 11)),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    pauseRecord();
+                  },
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  child: Container(
+                    height: 48.0,
+                    decoration: BoxDecoration(color: Colors.green.shade300),
+                    child: Center(
+                      child: Text(
+                        'stop',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    stopRecord();
+                    reset();
+                  },
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(
+              statusText,
+              style: TextStyle(color: Colors.red, fontSize: 20),
+            ),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              play();
+            },
+            child: Container(
+              margin: EdgeInsets.only(top: 30),
+              alignment: AlignmentDirectional.center,
+              width: 100,
+              height: 50,
+              child: isComplete && recordFilePath != null
+                  ? Text(
+                      "play",
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    )
+                  : Container(),
+            ),
+          ),
+          Center(child: buildTime()),
+        ]),
+      ),
+    );
+  }
+
+  Future<bool> checkPermission() async {
+    if (!await Permission.microphone.isGranted) {
+      PermissionStatus status = await Permission.microphone.request();
+      if (status != PermissionStatus.granted) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void startRecord() async {
+    checkPermission().then((allow) {
+      if (allow) {
+        statusText = "Recording...";
+        getFilePath().then((value) {
+          recordFilePath = value;
+          RecordMp3.instance.start(value, (type) {
+            statusText = "Record error--->$type";
+            setState(() {});
+          });
+        });
+        isComplete = false;
+
+        setState(() {});
+      }
+    });
+  }
+
+  void pauseRecord() {
+    if (RecordMp3.instance.status == RecordStatus.PAUSE) {
+      bool s = RecordMp3.instance.resume();
+      if (s) {
+        statusText = "Recording...";
+        setState(() {});
+      }
+    } else {
+      bool s = RecordMp3.instance.pause();
+      if (s) {
+        statusText = "Recording pause...";
+        setState(() {});
+      }
+    }
+  }
+
+  void stopRecord() {
+    bool s = RecordMp3.instance.stop();
+    if (s) {
+      statusText = "Record complete";
+      isComplete = true;
+      setState(() {});
+    }
+  }
+
+  void resumeRecord() {
+    bool s = RecordMp3.instance.resume();
+    if (recordFilePath != null && File(recordFilePath!).existsSync()) {
+      AudioPlayer audioPlayer = AudioPlayer()..setUrl(recordFilePath!);
+      audioPlayer.play();
+    }
+  }
+
+  String? recordFilePath;
+
+  void play() {
+    AudioPlayer audioPlayer = AudioPlayer()..setUrl(recordFilePath!);
+    audioPlayer.play();
+  }
+
+  int i = 0;
+
+  Future<String> getFilePath() async {
+    Directory storageDirectory = await getApplicationDocumentsDirectory();
+    String sdPath = storageDirectory.path + "/record";
+    var d = Directory(sdPath);
+    if (!d.existsSync()) {
+      d.createSync(recursive: true);
+    }
+    return sdPath + "/test_${i++}.mp3";
+  }
+}
